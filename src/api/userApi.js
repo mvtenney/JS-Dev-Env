2f30fcdcd78c4649dbd23f6062d09cd050c90009
Michael Tenney <Michael.Tenney@academicpartnerships.com>
2017-12-27 11:27:30 -0600

    Added Mock API

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,15 +1,33 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers() {
-    return get('users');
+  return get('users');
+}
+
+export function deleteUser(id) {
+  return del(`users/${id}`);
 }
 
 function get(url) {
-   return fetch(url).then(onSuccess, onError); 
+  return fetch(baseUrl + url).then(onSuccess, onError);
 }
 
-function onSuccess(response) {return response.json();}
+// Can't call func delete since reserved word.
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
 
-function onError(error){
-    console.log(error); //eslint-disable-line no-console
-};
+  return fetch(request).then(onSuccess, onError);
+}
+
+function onSuccess(response) {
+  return response.json();
+}
+
+function onError(error) {
+  console.log(error); // eslint-disable-line no-console
+}
