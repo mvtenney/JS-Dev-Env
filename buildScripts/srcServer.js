2d74b7bd7aa7a548db395137ba69bd6245047fe9
Michael Tenney <michaeltenney@live.com>
2017-12-27 07:57:22 -0600

    random commit

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,15 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  //Hard coding for simplicity. Pretend this is a real database.
+  res.json([
+    {"id":1,"firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+    {"id":2,"firstName":"Jack", "lastName":"Smith", "email":"jack@gmail.com"},
+    {"id":3,"firstName":"Tom", "lastName":"Smith", "email":"tom@gmail.com"},
+  ]);
+});
+
 app.listen(port, function(err){
   if(err) {
     console.log(err);
